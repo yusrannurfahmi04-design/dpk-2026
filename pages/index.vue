531f1dc4b505bd0c3ac204788899e76b5dc930b4
yusrannurfahmi04-design <yusrannurfahmi04@gmail.com>
2025-12-30 19:06:28 +0800

    Add shadow to UCard and wrap UButton in a div

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <UContainer class="py-20 text-center">
-    <UCard class="max-w-md mx-auto">
+    <UCard class="max-w-md mx-auto shadow-xl">
       <h1 class="text-3xl font-bold text-primary-600 mb-4">DPK Monitoring 2026</h1>
       <p class="text-gray-600 mb-8">Selamat datang di sistem monitoring pipeline Dana Pihak Ketiga.</p>
-      <UButton to="/input" size="xl" color="primary" block icon="i-heroicons-pencil-square">
-        Input Data Pipeline
-      </UButton>
+      <div class="space-y-4">
+        <UButton to="/input" size="xl" color="primary" block icon="i-heroicons-pencil-square">
+          Input Data Pipeline
+        </UButton>
+      </div>
     </UCard>
   </UContainer>
 </template>

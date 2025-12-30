e24941759aa450f770ac783bf89a1e9ac192a3f2
yusrannurfahmi04-design <yusrannurfahmi04@gmail.com>
2025-12-30 18:59:54 +0800

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <UContainer class="py-20 text-center">
+    <UCard class="max-w-md mx-auto">
+      <h1 class="text-3xl font-bold text-primary-600 mb-4">DPK Monitoring 2026</h1>
+      <p class="text-gray-600 mb-8">Selamat datang di sistem monitoring pipeline Dana Pihak Ketiga.</p>
+      <UButton to="/input" size="xl" color="primary" block icon="i-heroicons-pencil-square">
+        Input Data Pipeline
+      </UButton>
+    </UCard>
+  </UContainer>
+</template>

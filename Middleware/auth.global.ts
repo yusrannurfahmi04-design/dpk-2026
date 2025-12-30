107303f79fdcb35426c0cdb30bfbb2ec35c50e6a
yusrannurfahmi04-design <yusrannurfahmi04@gmail.com>
2025-12-31 06:45:23 +0800

    Update auth.global.ts

--- Middleware/auth.global.ts
+++ Middleware/auth.global.ts
@@ -1,13 +1,8 @@
 export default defineNuxtRouteMiddleware((to) => {
   const user = useSupabaseUser()
 
-  // Jika belum login dan mencoba buka halaman selain /login, arahkan ke /login
+  // Jika belum login dan buka selain /login, paksa ke /login
   if (!user.value && to.path !== '/login') {
     return navigateTo('/login')
-  }
-
-  // Jika sudah login dan mencoba buka halaman /login, arahkan ke dashboard
-  if (user.value && to.path === '/login') {
-    return navigateTo('/dashboard')
   }
 })

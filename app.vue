fdace5e092d2a69d237671fda161736407270aab
yusrannurfahmi04-design <yusrannurfahmi04@gmail.com>
2025-12-30 19:00:36 +0800

    Add navigation bar to app.vue

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <nav class="bg-black text-white p-4 shadow-md">
+      <UContainer class="flex justify-between items-center">
+        <NuxtLink to="/" class="text-xl font-bold">MONITORING DPK</NuxtLink>
+        <div class="space-x-4">
+          <UButton to="/" variant="ghost" color="white">Home</UButton>
+          <UButton to="/input" variant="solid" color="primary">Input Pipeline</UButton>
+        </div>
+      </UContainer>
+    </nav>
+    
+    <NuxtPage />
+  </div>
+</template>
